13407d17e31fec5ac8d8d0e6a55e3ac2ea839bd2
Timchik14 <temurtheg@rambler.ru>
2022-05-19 19:44:24 +0500

    C-6 Cars save functions \ Add migrations

--- app/Services/CarsService.php
+++ app/Services/CarsService.php
@@ -2,6 +2,10 @@
 
 namespace App\Services;
 
+use App\Models\Car;
+use Exception;
+use Illuminate\Http\JsonResponse;
+
 class CarsService
 {
     public function getCarType($car)
@@ -28,26 +32,23 @@ class CarsService
             'load_capacity' => $car['Load_capacity'],
             'holding_capacity' => 0,
             'glonass' => '',
-            'pts' => $car['Current_documents'][0]['Key']['Type'],
-            'sts' => $car['Current_documents'][0]['Key']['Type'],
-            'lease' => '',
+            'pts' => json_encode(['json' => $car['Current_documents'][0]['Key']['Type']]),
+            'sts' => json_encode(['json' => $car['Current_documents'][0]['Key']['Type']]),
+            'lease' => '{}',
             'created_by' => 1,
             'type' => $carType,
             'company_id' => 1,
-            'status' => 1,
             'updated_by' => 1,
             'report_uid' => '',
             'unloaded_weight' => $car['Curb_weight'],
-            'country_id' => 1,
-            'is_non_resident' => false,
             'dd_id' => 1,
             'external_id' => $car['Key']['ExternalID'],
             'temperature_recorder' => '',
-            'tracking_number ' => '',
-            'sts_number ' => $car['Current_documents'][0]['Key']['ExternalID'],
+            'tracking_number' => '',
+            'sts_number' => $car['Current_documents'][0]['Key']['ExternalID'],
             'fines_monitoring_id' => 0,
-            'owner_confirmation_status ' => 0,
-            'platform_type  ' => 0,
+            'owner_confirmation_status' => 0,
+            'platform_type' => 0,
             'owner_id' => 1,
             'owner_inn' => '',
             'owner_entity_type' => 1,
@@ -67,5 +68,43 @@ class CarsService
         $reply['Error_text'] = $response['errorText'];
 
         return $reply;
+    }
+
+    public function insert($car, $data)
+    {
+        if ($car['Key']['Type'] == 'vehicles') {
+            try {
+                Car::upsert($data, ['external_id']);
+                $response['errorCode'] = 1;
+                $response['errorText'] = '';
+            } catch (Exception $e) {
+                $response['errorCode'] = 2;
+                $response['errorText'] = $e->getMessage();
+            }
+        }
+        return $response;
+    }
+
+    public function insertData($request)
+    {
+        $message = json_decode($request['json'], true);
+        $responses['Head'] = ['Source' => 'AT', 'MessageId' => '881e1e3a-309b-11eb-969d-00505695dcc5'];
+
+        foreach ($message['Message']['Body']['Objects'] as $car) {
+
+            if ($car['Is_deleted']) {
+                Car::where('external_id', $car['Key']['ExternalID'])->delete();
+            } else {
+                Car::withTrashed()
+                    ->where('external_id', $car['Key']['ExternalID'])
+                    ->restore();
+
+                $data = $this->prepareData($car);
+                $response = $this->insert($car, $data);
+                $responses['Body']['Reply'][] = $this->prepareReply($response, $data);
+            }
+        }
+
+        return new JsonResponse(['Message' => $responses], 200);
     }
 }
---
+++ database/migrations/2022_05_19_123127_create_cars_table.php
@@ -0,0 +1,81 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('number', 255);
+            $table->string('mark', 255)->nullable(false);
+            $table->string('model', 255);
+            $table->string('color', 255);
+            $table->string('vin', 255);
+            $table->decimal('load_capacity', 12, 2);
+            $table->integer('holding_capacity');
+            $table->string('glonass', 255);
+            $table->json('pts');
+            $table->json('sts');
+            $table->json('lease');
+            $table->unsignedBigInteger('created_by');
+            $table->smallInteger('type')->default(1)->nullable(false);
+            $table->unsignedBigInteger('company_id');
+            $table->integer('status')->default(1)->nullable(false);
+            $table->integer('updated_by');
+            $table->string('report_uid', 255);
+            $table->decimal('unloaded_weight', 6, 3);
+            $table->integer('country_id')->default(0)->nullable(false);
+            $table->boolean('is_non_resident')->default(false)->nullable(false);
+            $table->unsignedBigInteger('dd_id');
+            $table->string('external_id', 255)->unique();
+            $table->string('temperature_recorder', 255);
+            $table->string('tracking_number', 255);
+            $table->string('sts_number', 255)->default('')->nullable(false);
+            $table->integer('fines_monitoring_id');
+            $table->smallInteger('owner_confirmation_status');
+            $table->smallInteger('platform_type');
+            $table->unsignedBigInteger('owner_id');
+            $table->string('owner_inn', 255);
+            $table->smallInteger('owner_entity_type');
+
+            $table
+                ->foreign('created_by')
+                ->references('id')
+                ->on('users');
+            $table
+                ->foreign('company_id')
+                ->references('id')
+                ->on('companies');
+            $table
+                ->foreign('dd_id')
+                ->references('id')
+                ->on('tms_nodes');
+            $table
+                ->foreign('owner_id')
+                ->references('id')
+                ->on('companies');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
